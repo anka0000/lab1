79c491c3fbf1077533c74505718feef1a4ae9479
anka0000 <akurtyzhova@mail.ru>
2025-10-15 07:14:46 +0000

    лабораторная 2

---
+++ lab2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <limits.h>
+long long arkerman (int m, int n) {
+	if (m == 0) {
+		return n+1;
+	} else if (n == 0) {
+		return arkerman(m - 1, 1);
+	} else {
+		return arkerman(m - 1, arkerman(m, n - 1));
+	}
+}
+int isvalidinput (int m, int n) {
+	if (m < 0 || n < 0) {
+		return 0;
+	}
+}
+int main() {
+	int m;
+	int n;
+	scanf("%d", &m);
+	scanf("%d", &n);
+	if (!isvalidinput(m, n)) {
+		return 1;
+	}
+long long resultat = arkerman(m, n);
+	printf("%lld\n", resultat);
+		return 0;
+}
